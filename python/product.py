628157918d297b14b200d1e8fdf90fbd9193bad7
AntonioFarias <antoniotercerio@gmail.com>
2022-10-01 09:54:03 -0300

    feat: getter-and-setter-price

--- python/product.py
+++ python/product.py
@@ -18,4 +18,17 @@ class Product:
         if not isinstance(cod,str):
             print('The code is wrong, try again!')
             return
-        self._code = cod 
+        self._code = cod
+
+    @property
+    def price(self):
+        return self._price
+
+    #Code setter
+    @code.setter 
+    def price(self,price_in):
+        if not isinstance(price_in,str):
+            print('The price is wrong, try again!')
+            return
+        self._price = price_in
+
