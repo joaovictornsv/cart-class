86378d4c02909207598ea4700580f0e69d356e63
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2022-10-01 17:04:08 -0300

    Implement cart methods

--- java/Cart.java
+++ java/Cart.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
+
 public class Cart {
     ArrayList<Product> products = new ArrayList<>();
 
 	public Cart() {
 
 	}
+
+    public void add_product(Product product) {
+		products.add(product);
+	}
+
+	public void remove_product(String product_code) {
+		for (int i = 0; i < products.size(); i++) {
+			if (products.get(i).getCode() == product_code) {
+				products.remove(i);
+			}
+		}
+	}
+
+	public Double calculate_price() {
+		Double price = 0.0;
+		
+		for (int i = 0; i < products.size(); i++) {
+			price += products.get(i).getPrice();
+		}
+		
+		return price;
+	}
+	
+	public Integer total_itens() {
+		return products.size();
+	}
+	
+	public ArrayList<Product> get_itens() {
+		return products;
+	}
+	
+	public void clear_cart() {
+		products.clear();
+	}
+	
+
 }
