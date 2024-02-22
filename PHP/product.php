dba356aa4d9851fea12e57bba6b0339cb40c3599
lchampz <joca11victor@gmail.com>
2024-02-22 12:06:59 -0300

    feat: adicionando implementação da classe cart

--- PHP/product.php
+++ PHP/product.php
@@ -1,27 +1,29 @@
-class Product{
+<?php
+class Product
+{
   public string $name;
   public string $code;
   public int $price;
 
   public function __construct(string $name, string $code, int $price)
-    {
-        $this->name = $name; 
-        $this->code = $code; 
-        $this->price = $price; 
-    }
+  {
+    $this->name = $name;
+    $this->code = $code;
+    $this->price = $price;
+  }
 
-  public function getName(): string 
-    {
-        return $this->name;
-    }
+  public function getName(): string
+  {
+    return $this->name;
+  }
 
-  public function getCode(): string 
-    {
-        return $this->code;
-    }
+  public function getCode(): string
+  {
+    return $this->code;
+  }
 
-  public function getPrice(): string 
-    {
-        return $this->price;
-    }
+  public function getPrice(): string
+  {
+    return $this->price;
+  }
 }
